fda38e664b77f7e33f0cd404645299896ad54606
Code Sandbox <hello@codesandbox.io>
2021-09-24 09:51:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.35afb15317f10adb64f57073f5387760.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c405a58166b1052d1091c08d18bea5",
+    "url": "https://.github.io/csb-soykg/index.html"
+  },
+  {
+    "revision": "365951f6b9c190d169db",
+    "url": "https://.github.io/csb-soykg/static/css/2.dfd58508.chunk.css"
+  },
+  {
+    "revision": "b4bacb3b77daa5742ffc",
+    "url": "https://.github.io/csb-soykg/static/css/main.a11625c5.chunk.css"
+  },
+  {
+    "revision": "365951f6b9c190d169db",
+    "url": "https://.github.io/csb-soykg/static/js/2.ffbd831f.chunk.js"
+  },
+  {
+    "revision": "b4bacb3b77daa5742ffc",
+    "url": "https://.github.io/csb-soykg/static/js/main.279496b7.chunk.js"
+  },
+  {
+    "revision": "f190f53b5f3de6f4aa4e",
+    "url": "https://.github.io/csb-soykg/static/js/runtime~main.4b5aeec4.js"
+  },
+  {
+    "revision": "3f2a4daede60a2fd953c7dd31fa71972",
+    "url": "https://.github.io/csb-soykg/static/media/burger.3f2a4dae.png"
+  },
+  {
+    "revision": "65fd5b77ad79d908b38f1b913c392140",
+    "url": "https://.github.io/csb-soykg/static/media/ground.65fd5b77.png"
+  },
+  {
+    "revision": "ec3df7229d04b694254033fd6285ac9a",
+    "url": "https://.github.io/csb-soykg/static/media/houses.ec3df722.png"
+  },
+  {
+    "revision": "746323a4ffd8540c15beb78e0ff61eec",
+    "url": "https://.github.io/csb-soykg/static/media/kitchen.746323a4.png"
+  },
+  {
+    "revision": "5a4af241bd236b06331ab0a2f8c1fbee",
+    "url": "https://.github.io/csb-soykg/static/media/plate.5a4af241.png"
+  },
+  {
+    "revision": "20e01a93f886b54ddf3986b6d316317d",
+    "url": "https://.github.io/csb-soykg/static/media/room.20e01a93.png"
+  },
+  {
+    "revision": "898621f780a0ccca7c8ebc3ff182b451",
+    "url": "https://.github.io/csb-soykg/static/media/room.898621f7.png"
+  },
+  {
+    "revision": "e53fae9282e91168d26a52a165ff960e",
+    "url": "https://.github.io/csb-soykg/static/media/room.e53fae92.png"
+  },
+  {
+    "revision": "436bbe5a58b96c85ca57494db02dff6e",
+    "url": "https://.github.io/csb-soykg/static/media/signboard.436bbe5a.png"
+  },
+  {
+    "revision": "b17e6031047271a8bf57c6d4ea298ceb",
+    "url": "https://.github.io/csb-soykg/static/media/table.b17e6031.png"
+  },
+  {
+    "revision": "aa4e1286ab429892c5fd0ec0b268dadb",
+    "url": "https://.github.io/csb-soykg/static/media/winner.aa4e1286.png"
+  },
+  {
+    "revision": "6a9cdb6b1f93f38913529f0ee73d31f4",
+    "url": "https://.github.io/csb-soykg/static/media/wooden-floor.6a9cdb6b.png"
+  }
+]);
